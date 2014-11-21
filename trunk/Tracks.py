5a315f335b3e5c1189d0cf1e10db1a123cc98d12
ivanovandrew2004@gmail.com <ivanovandrew2004@gmail.com@eee7c4ba-b005-0bfe-c914-50cc8e81d5dd>
2014-11-21 23:56:25 +0000

    Changed drawing of straight lines. Changed image size. Changed source tracks. Added colors.

--- trunk/Tracks.py
+++ trunk/Tracks.py
@@ -3,13 +3,25 @@ __author__ = 'Andrew'
 import numpy as np
 import cv2
 
-BLACK = 0
-GRAY = 128
-RED = 200
-WHITE = 255
-IMAGE_SIZE = 600
-FRAMES_COUNT = 10
+BLACK = [0, 0, 0]
+GRAY = [128, 128, 128]
+RED = [0, 0, 255]
+BLUE = [255, 0, 0]
+GREEN = [0, 255, 0]
+WHITE = [255, 255, 255]
+
+number_to_color = dict([(0, GRAY), (1, GRAY),(2, GRAY),(3, GRAY),(4, GRAY),(5, GRAY),
+                        (6, BLUE),(7, BLUE),(8, BLUE), (9, BLUE),(10, BLUE),(11, BLUE),
+                        (12, GREEN),(13, GREEN),(14, GREEN),(15, GREEN),(16, GREEN),(17, GREEN),(18, GREEN),
+                        (19, RED)])
+
+FRAMES_COUNT = 50
 GOOD_TRACKS_COUNT = 20
+LAST_POINTS = 5
+
+OBJECT_SIZE = 3
+
+IMAGE_SIZE = FRAMES_COUNT * OBJECT_SIZE * 4 + 3
 IMAGE_NAME = "frame_"
 EXTENSION = ".png"
 
@@ -19,40 +31,44 @@ third = []
 fourth = []
 fifth = []
 
-# region source map creating
+# region Image creating
 def create_map():
-    image = np.empty([IMAGE_SIZE, IMAGE_SIZE], dtype=type(int))
+
+    image = np.empty([IMAGE_SIZE, IMAGE_SIZE, 3], dtype=int)
     for i in range(IMAGE_SIZE):
         for j in range(IMAGE_SIZE):
             image[i, j] = BLACK
     return image
 # endregion
 
-# region source tracks
-def get_next_coordinates_1(t):
+# region Image saving
+def save_image(image, suffix):
 
-    return 5*t + 1, IMAGE_SIZE/2 - t/4
-
-def get_next_coordinates_2(t):
-
-    return 5*t + 1, IMAGE_SIZE/2 + t/4
+    str_suffix = str(suffix)
+    if len(str_suffix) == 1:
+        str_suffix = "0" + str_suffix
+    name = IMAGE_NAME + str_suffix + EXTENSION
+    cv2.imwrite(name, image)
 # endregion
 
-def draw_straight_line(image, one, two, color):
-    x0, y0 = one
-    x1, y1 = two
+# region Drawing object points
+def fill_object_points(image):
+    point1, point2 = first
+    point3, point4 = second
+    point5, point6 = third
+    point7, point8 = fourth
+    point9, point10 = fifth
 
-    delta_x = x1 - x0
-    step = (y1 - y0)/float(delta_x)
+    list = [point1, point2, point3, point4, point5, point6, point7, point8, point9, point10]
 
-    for i in range(delta_x + 1):
-        x = x0 + i
-        y = y0 + int(step * i)
+    for point in list:
+        plot_object_point(image, point)
 
-        plot_point(image, (x, y), color)
+    return image
 
-def plot_point(image, point, color):
+def plot_object_point(image, point):
 
+    color = WHITE
     x, y = point
     image[x - 1, y - 1] = color
     image[x - 1, y] = color
@@ -65,15 +81,66 @@ def plot_point(image, point, color):
     image[x + 1, y - 1] = color
     image[x + 1, y] = color
     image[x + 1, y + 1] = color
+# endregion
 
-def update_points(one, two, three, four, five, newPoint):
+# region Drawing tracks
+def plot_track_point(image, point, color):
 
-    one = two
-    two = three
-    three = four
-    four = five
-    five = newPoint
-    return one, two, three, four, five
+    x, y = point
+    image[x, y] = color
+
+def draw_straight_line(image, one, two, color):
+    x_start, y_start = one
+    x_end, y_end = two
+
+    x0, y0 = x_start, y_start
+
+    delta_x = x_end - x_start
+    delta_y = y_end - y_start
+
+    if np.abs(delta_x) > np.abs(delta_y):
+        step = delta_y/float(delta_x)
+
+        for i in range(1, np.abs(delta_x)):
+            x = x0 + i
+            y = y0 + int(step * i)
+            # print (x, y)
+
+            plot_track_point(image, (x, y), color)
+
+    else:
+        step = delta_x / float(delta_y)
+
+        if step < 0:
+            y0 = y_end
+
+        for i in range(1, np.abs(delta_y)):
+            x = x0 + int(step * i)
+            y = y0 + i
+            # print (x, y)
+
+            plot_track_point(image, (x, y), color)
+
+def draw_tracks(tracks):
+
+    image = create_map()
+    count = 0
+
+    for track in tracks:
+        color = number_to_color.get(count)
+
+        point1, point2, point3, point4, point5 = track
+        draw_straight_line(image, point1, point2, color)
+        draw_straight_line(image, point2, point3, color)
+        draw_straight_line(image, point3, point4, color)
+        draw_straight_line(image, point4, point5, color)
+
+        count += 1
+
+    image = fill_object_points(image)
+
+    return image
+# endregion
 
 # region Hypothesis
 def calculate_sum(vector):
@@ -159,46 +226,32 @@ def generate_hypothesis():
         result.append(points)
         count += 1
 
+    result.reverse()
     return result
 # endregion
 
-def save_image(image, suffix):
-    name = IMAGE_NAME + str(suffix) + EXTENSION
-    cv2.imwrite(name, image)
+# region source tracks
+def get_next_coordinates_1(t):
 
-def draw_tracks(tracks):
+    return t * (OBJECT_SIZE * 4) + 1, IMAGE_SIZE/2 - t * (OBJECT_SIZE * 2)
 
-    is_first = True
-    image = create_map()
+def get_next_coordinates_2(t):
 
-    for track in tracks:
-        color = GRAY
-        if is_first:
-            color = RED
-            is_first = False
-
-        point1, point2, point3, point4, point5 = track
-        draw_straight_line(image, point1, point2, color)
-        draw_straight_line(image, point2, point3, color)
-        draw_straight_line(image, point3, point4, color)
-        draw_straight_line(image, point4, point5, color)
-
-    return image
+    return t * (OBJECT_SIZE * 4) + 1, IMAGE_SIZE/2 + t * (OBJECT_SIZE * 2)
+# endregion
 
 for i in range(FRAMES_COUNT):
     print "##### ", i, "starts #####"
     point1 = get_next_coordinates_1(i)
     point2 = get_next_coordinates_2(i)
 
-    first, second, third, fourth, fifth = \
-        update_points(first, second, third, fourth, fifth, (point1, point2))
+    first, second, third, fourth, fifth = second, third, fourth, fifth, (point1, point2)
 
-    print "first = ", first
-    print "second = ", second
-    good_tracks = generate_hypothesis()
+    if i >= LAST_POINTS:
+        good_tracks = generate_hypothesis()
 
-    image = draw_tracks(good_tracks)
+        image = draw_tracks(good_tracks)
 
-    save_image(image, i)
+        save_image(image, i)
 
     print "#####", i, "ends #####"
