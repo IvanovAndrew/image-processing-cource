62de6b3bb942a9dedc863d253a9a0aaeebd7a1d4
ivanovandrew2004@gmail.com <ivanovandrew2004@gmail.com@eee7c4ba-b005-0bfe-c914-50cc8e81d5dd>
2014-12-04 13:57:18 +0000

    added Sobel. Yet not working

--- trunk/task 4/Simple_stereo.py
+++ trunk/task 4/Simple_stereo.py
@@ -7,27 +7,28 @@ import cv2
 import cv2 as cv
 
 RESULT_FILE_NAME = "result.txt"
-DERIVATION_THRESHOLD = 20
-DELTA = 1
+DELTA = 2
 BIG_VALUE = 10000
-MATCHING_THRESHOLD = 100 * DELTA
+MATCHING_THRESHOLD = 200 * DELTA
+PIXEL_THRESHOLD = 50
 
 fst_image = cv2.imread("im2.png", cv2.CV_LOAD_IMAGE_GRAYSCALE)
 snd_image = cv2.imread("im6.png", cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
 s_x = fst_image.shape[0]
 s_y = fst_image.shape[1]
-total_count = 0
 
 print fst_image.shape
 
 # region file manipulations
 def create_file (fileName):
+
     file = open(fileName, "w")
     file.write("")
     file.close()
 
 def write_to_file (fileName, x, y, z):
+
     file = open (fileName, "a")
     file.write (str(x) + " " + str(y) + " " + str(z) + "\n")
     file.close()
@@ -44,7 +45,7 @@ def calculate_x_y_z(u, v, d):
 
 def get_pixel_with_neighs(image, x, y):
 
-    result = np.zeros([2 * DELTA + 1], dtype=np.uint8)
+    result = np.zeros([2 * DELTA + 1], dtype=np.float32)
 
     for i in range(2 * DELTA + 1):
         shift = i - DELTA
@@ -63,43 +64,25 @@ def analyse_min(line):
         if num[0] < fst_min:
             snd_min = fst_min
             fst_min = num[0]
-            min_index = index + 1
+            min_index = index
         elif num[0] < snd_min:
             snd_min = num[0]
 
     if np.abs(snd_min - fst_min) <= MATCHING_THRESHOLD:
         min_index = -1
-    else:
-        print "fst_min =", fst_min, "snd_min =", snd_min
 
     return min_index
-
-def to_first_derivation(image):
-
-    row, columns = image.shape
-
-    result = np.zeros([row, columns - 1], dtype=np.uint8)
-
-    for i in range(row):
-        for j in range(1, columns):
-            value = np.float(image[i, j]) - np.float(image[i, j - 1])
-            if value <= DERIVATION_THRESHOLD:
-                result[i, j - 1] = 0
-            else:
-                result[i, j - 1] = 255##int(value)
-
-    return result
 
 def find_bounds(one, two):
     rows, columns = one.shape
     for i in range(rows):
+        print i, "of", rows
+
         for j in range(columns - DELTA):
             if j < DELTA:
                 continue
 
-            # diff = np.float(one[i, j]) - np.float(one[i, j - 1])
-            # if np.abs(diff) >= DERIVATION_THRESHOLD:
-            if one[i, j] > 0:
+            if one[i, j] > PIXEL_THRESHOLD:
                 line = two[i, :]
                 template = get_pixel_with_neighs(one, i, j)
 
@@ -110,6 +93,32 @@ def find_bounds(one, two):
                     x, y, z = calculate_x_y_z(i, j, shift)
                     write_to_file(RESULT_FILE_NAME, x, y, z)
 
-one = to_first_derivation(fst_image)
-two = to_first_derivation(snd_image)
+one = cv2.Sobel(fst_image, cv2.CV_32F, 1, 0, ksize=3)
+two = cv2.Sobel(snd_image, cv2.CV_32F, 1, 0, ksize=3)
+
+one_32F = cv2.Sobel(fst_image, cv2.CV_32F, 1, 0, ksize=3)
+cv2.imwrite("im2 after Sobel CV_32F.png", cv2.cvtColor(one_32F, cv2.COLOR_GRAY2BGR))
+
+one_8U = cv2.Sobel(fst_image, cv2.CV_32F, 1, 0, ksize=3)
+cv2.imwrite("im2 after Sobel CV_8U.png", cv2.cvtColor(one_8U, cv2.COLOR_GRAY2BGR))
+
+count = 0
+sum = 0
+for i in range(s_x):
+    for j in range(s_y):
+         if one[i, j] > 0:
+             count += 1
+             sum += one[i, j]
+print "total", s_x * s_y
+print "count", count, "average: ", sum/float(count)
+
+for i in range(s_x):
+    for j in range(s_y):
+         if two[i, j] > 0:
+             count += 1
+             sum += two[i, j]
+print "total", s_x * s_y
+print "count", count, "average: ", sum/float(count)
+
+
 find_bounds(one, two)
