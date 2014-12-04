41051348025a1ff5b0c8cfb01abb0755fda8f885
ivanovandrew2004 <ivanovandrew2004@gmail.com>
2014-12-04 00:10:16 +0000

    added 4th task. Yet not working

--- task 4/Simple_stereo.py
+++ task 4/Simple_stereo.py
@@ -1,16 +1,25 @@
+from cv2.cv import CV_TM_SQDIFF
+
 __author__ = 'Andrew'
 
 import numpy as np
 import cv2
+import cv2 as cv
 
 RESULT_FILE_NAME = "result.txt"
+DERIVATION_THRESHOLD = 20
+DELTA = 1
+BIG_VALUE = 10000
+MATCHING_THRESHOLD = 100 * DELTA
 
-fst_image = cv2.cvtColor(cv2.imread("im2.png"), cv2.COLOR_BGR2GRAY)
-snd_image = cv2.cvtColor(cv2.imread("im6.png"), cv2.COLOR_BGR2GRAY)
+fst_image = cv2.imread("im2.png", cv2.CV_LOAD_IMAGE_GRAYSCALE)
+snd_image = cv2.imread("im6.png", cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
 s_x = fst_image.shape[0]
 s_y = fst_image.shape[1]
+total_count = 0
 
+print fst_image.shape
 
 # region file manipulations
 def create_file (fileName):
@@ -33,12 +42,74 @@ def calculate_x_y_z(u, v, d):
 
     return x, y, z
 
-def analyse_line(num):
-    for j in range(s_y):
-        if j == 0:
-            continue
+def get_pixel_with_neighs(image, x, y):
 
-        if (fst_image[j])
+    result = np.zeros([2 * DELTA + 1], dtype=np.uint8)
 
-for line in range(s_x):
-    analyse_line(line)
+    for i in range(2 * DELTA + 1):
+        shift = i - DELTA
+        result[i] = image[x, y + shift]
+
+    return result
+
+def analyse_min(line):
+
+    min_index = -1
+    fst_min = BIG_VALUE
+    snd_min = BIG_VALUE
+
+    for index in range(len(line)):
+        num = line[index]
+        if num[0] < fst_min:
+            snd_min = fst_min
+            fst_min = num[0]
+            min_index = index + 1
+        elif num[0] < snd_min:
+            snd_min = num[0]
+
+    if np.abs(snd_min - fst_min) <= MATCHING_THRESHOLD:
+        min_index = -1
+    else:
+        print "fst_min =", fst_min, "snd_min =", snd_min
+
+    return min_index
+
+def to_first_derivation(image):
+
+    row, columns = image.shape
+
+    result = np.zeros([row, columns - 1], dtype=np.uint8)
+
+    for i in range(row):
+        for j in range(1, columns):
+            value = np.float(image[i, j]) - np.float(image[i, j - 1])
+            if value <= DERIVATION_THRESHOLD:
+                result[i, j - 1] = 0
+            else:
+                result[i, j - 1] = 255##int(value)
+
+    return result
+
+def find_bounds(one, two):
+    rows, columns = one.shape
+    for i in range(rows):
+        for j in range(columns - DELTA):
+            if j < DELTA:
+                continue
+
+            # diff = np.float(one[i, j]) - np.float(one[i, j - 1])
+            # if np.abs(diff) >= DERIVATION_THRESHOLD:
+            if one[i, j] > 0:
+                line = two[i, :]
+                template = get_pixel_with_neighs(one, i, j)
+
+                result = cv.matchTemplate(line, template, CV_TM_SQDIFF)
+                index = analyse_min(result)
+                if index >= 0 and j != index:
+                    shift = index - j
+                    x, y, z = calculate_x_y_z(i, j, shift)
+                    write_to_file(RESULT_FILE_NAME, x, y, z)
+
+one = to_first_derivation(fst_image)
+two = to_first_derivation(snd_image)
+find_bounds(one, two)
